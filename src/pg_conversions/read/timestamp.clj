ef28605aa26748b6752a5b72db884013692efd41
Erik Strömberg <erik.stromberg@gmail.com>
2015-08-04 15:15:34 +0200

    .

--- src/pg_conversions/read/timestamp.clj
+++ src/pg_conversions/read/timestamp.clj
@@ -1,6 +1,6 @@
 (ns pg-conversions.read.timestamp
   (:require [clojure.java.jdbc :as jdbc]
-            [clj-time.coerce :refer [from-sql-time]
+            [clj-time.coerce :refer [from-sql-time]]))
 
 (extend-protocol jdbc/IResultSetReadColumn
   java.sql.Timestamp
