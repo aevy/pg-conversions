d399a76a3e3b7039122eb07f7697cc6a12f41f2c
Erik Strömberg <erik.stromberg@gmail.com>
2015-08-12 15:05:03 +0200

    .

--- src/pg_conversions/core.clj
+++ src/pg_conversions/core.clj
@@ -5,7 +5,7 @@
   (:import [org.postgresql.util PGobject]))
 
 (defn uuid? [s]
-  (re-find #"^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$" s))
+  (re-find #"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$" s))
 
 (extend-protocol jdbc/ISQLValue
   java.util.Date
